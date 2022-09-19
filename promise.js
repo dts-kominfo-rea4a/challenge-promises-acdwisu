54b99e7cd2b89cdb3d7db1f22ca933c6af33bc73
acdw <acdwisu@outlook.com>
2022-09-20 02:43:37 +0700

    promise emosi counter

--- promise.js
+++ promise.js
@@ -1,7 +1,23 @@
 const { promiseTheaterIXX, promiseTheaterVGC } = require("./external.js");
 
 // TODO: Buat fungsi promiseOutput sesuai ketentuan readme
-const promiseOutput = null;
+const promiseOutput = (emosi) => {
+  return new Promise(async (resolve, reject) => {
+    if(emosi == null || emosi.isEmpty) {
+      return reject('wrong input');
+    }
+
+    let result = await Promise.all([
+      promiseTheaterIXX(),
+      promiseTheaterVGC(),
+    ]);
+
+    return resolve(result.reduce(
+      (total, current) => total + current.filter((obj) => obj.hasil == emosi).length, 
+      0
+    ));
+  });
+};
 
 module.exports = {
   promiseOutput,
